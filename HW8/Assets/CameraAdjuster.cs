54a662fcff697915700b82249be4ac72ccbcaaf6
jinxiaol <wesleywu314@gmail.com>
2025-12-23 14:36:44 +0800

    1223

--- HW8/Assets/CameraAdjuster.cs
+++ HW8/Assets/CameraAdjuster.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 public class CameraAdjuster : MonoBehaviour
 {
-    // 設定你開發時的目標比例（例如 16:9 填 1.777f，4:3 填 1.333f）
-    // 如果不確定，就填 16f/9f
     float targetAspect = 16f / 9f;
 
     void Start()
@@ -14,7 +12,6 @@ public class CameraAdjuster : MonoBehaviour
 
         if (scaleHeight < 1.0f)
         {
-            // 如果螢幕太窄，增加上下黑邊（Letterbox）
             Rect rect = camera.rect;
             rect.width = 1.0f;
             rect.height = scaleHeight;
@@ -24,7 +21,6 @@ public class CameraAdjuster : MonoBehaviour
         }
         else
         {
-            // 如果螢幕太寬，增加左右黑邊（Pillarbox）
             float scaleWidth = 1.0f / scaleHeight;
             Rect rect = camera.rect;
             rect.width = scaleWidth;
